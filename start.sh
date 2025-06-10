a3115592a3820f9c91124db291dd3da1da6707ae
Ryan Edgell <redgell@us.ibm.com>
2025-06-10 15:09:43 -0500

    Adds transport option for stdio or sse

--- start.sh
+++ start.sh
@@ -2,4 +2,13 @@
 /usr/local/bin/ibmcloud config --check-version=false > /dev/null
 /usr/local/bin/ibmcloud login --apikey $IBMCLOUD_API_KEY -r us-south > /dev/null
 
-/usr/local/bin/ibmcloud --mcp-transport stdio --mcp-tools "$IBMCLOUD_MCP_TOOLS"
+case "$IBMCLOUD_MCP_TRANSPORT" in
+    sse)
+        transport_mode="http://0.0.0.0:4444"
+        ;;
+    *)
+        transport_mode="stdio"
+        ;;
+esac
+
+/usr/local/bin/ibmcloud --mcp-transport $transport_mode --mcp-tools $IBMCLOUD_MCP_TOOLS
